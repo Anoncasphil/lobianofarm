8086f8e3c333cf932cd63d4d3d340848d6fbb67b
Your Name <you@example.com>
2024-12-14 21:56:51 +0800

    for login

---
+++ landing page (customer)/mailer.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+$mail->Username ="lukemia19@gmail.com";
+$mail->Password ="rskaxydhoqtzjzwm";
+
+$mail->isHtml(true);
+
+return $mail;
+?>
