fe71692d3e0e2954ebf562c353f6152a54a24c32
Antoine Philipp Ochea <antoineochea0321@gmail.com>
2024-12-16 10:47:54 +0800

    changed functionality of event page and also completed the album page

--- events/events.php
+++ events/events.php
@@ -151,7 +151,7 @@
 								</button>
 
 								<!-- Archive Event Button -->
-								<button type="button" onclick="archiveEvent(<?php echo $eventId; ?>)" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+								<button type="button" onclick="archiveEvent(<?php echo $eventId; ?>); reloadPage()" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
 									<i class="fa-solid fa-box-archive"></i>
 								</button>
 							</div>
@@ -310,6 +310,7 @@
 	<script src="../path/to/flowbite/dist/flowbite.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 	<script src="../scripts/script.js"></script>
+	<script></script>
 
 </body>
 </html>
