86f866ddfca9e7089439e4f385c8f15cece5fc54
Astasheesh <Astasheesh>
2024-12-17 03:31:14 +0800

    das

--- reservation/send_payment.php
+++ reservation/send_payment.php
@@ -75,7 +75,7 @@ if (isset($_SESSION['first_name'])) {
             </div>
 
             <!-- Payment container -->
-            <div id="payment_container" class="w-[80%] h-full">
+            <div id="payment_container" class="w-[80%] h-full mt-[10%] ">
                 <div id="proof_payment_container" class="flex flex-row w-full h-[60%]">
                     <div id="attach_picture" class="flex flex-col justify-center w-[50%] h-full">
                         <p class="mb-1">Attach the proof of payment*</p>
@@ -85,13 +85,13 @@ if (isset($_SESSION['first_name'])) {
                             <div class="flex items-center justify-center w-full">
                                 <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 ">
                                     <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                                        <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                        <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400 z-20" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                                         </svg>
                                         <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
                                         <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                                     </div>
-                                    <input id="dropzone-file" type="file" name="file_farm" class="" />
+                                    <input id="dropzone-file" type="file" name="file_farm" class="z-10" />
                                 </label>
                             </div>
                         </form>
