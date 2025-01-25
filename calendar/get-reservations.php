9c8bda062e5928e6503f5f94a96fd194a0292e54
johncabuquin <johncabuquin02@gmail.com>
2025-01-26 06:18:09 +0800

    re: D -same day ND- highlight

--- calendar/get-reservations.php
+++ calendar/get-reservations.php
@@ -21,6 +21,8 @@ while ($row = $result->fetch_assoc()) {
     // Add 1 day to the check-in and check-out dates
     $checkInDate = new DateTime($row['reservation_check_in_date']);
     $checkOutDate = new DateTime($row['reservation_check_out_date']);
+    $checkInDate->modify('+1 day');
+    $checkOutDate->modify('+1 day'); // Add 1 day to the check-out date
 
     // Format the new dates back to string
     $checkInDateFormatted = $checkInDate->format('Y-m-d');
