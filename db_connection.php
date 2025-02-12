84d6dbdf0af55d9892c5a1c2955c3454766d19de
Antoine Philipp Ochea <antoineochea0321@gmail.com>
2025-02-12 14:32:47 +0800

    test

--- db_connection.php
+++ db_connection.php
@@ -1,13 +1,11 @@
 <?php
-$servername = "localhost"; // Replace with your actual MySQL hostname
-$username = "u157210740_lobianofarm"; // Your MySQL username
-$password = "Acast_1209"; // Replace with your actual password
-$database = "u157210740_lobianofarm"; // Your database name
+$host = "localhost"; 
+$dbname = "u157210740_lobianofarm";
+$username = "u157210740_lobianofarm";
+$password = "Acast_1209"; // Change this immediately for security
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
