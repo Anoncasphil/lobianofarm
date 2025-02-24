5c002e97d03a461fbd6488dbf98391143a56b311
Antoine Philipp Ochea <antoineochea0321@gmail.com>
2025-02-25 01:50:23 +0800

    new

--- db_connection.php
+++ db_connection.php
@@ -1,10 +1,10 @@
 <?php
-$host = "localhost";
-$user = "root"; // Default XAMPP MySQL user
-$pass = ""; // No password by default
-$dbname = "login"; // Your database name
+$host = "localhost"; 
+$dbname = "u157210740_lobianofarm";
+$username = "u157210740_lobianofarm";
+$password = "Acast_1209"; // Change this immediately for security
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
